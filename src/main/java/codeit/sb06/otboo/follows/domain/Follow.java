74cd5e92a7e515f38c00d0b7973af11d56325c88
SeoGyeongWon <skwknh00@naver.com>
2026-01-27 14:13:14 +0900

    feat: 팔로우 엔티티 작성

---
+++ src/main/java/codeit/sb06/otboo/follows/domain/Follow.java
@@ -0,0 +1,52 @@
+package codeit.sb06.otboo.follows.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "follows",
+uniqueConstraints = {
+  @UniqueConstraint(columnNames = {"follower_id", "followee_id"})
+  }
+)
+public class Follow {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "follower_id",nullable = false)
+  private UUID followerId;
+
+  @Column(name = "followee_id",nullable = false)
+  private UUID followeeId;
+
+
+  @Builder
+  public Follow(UUID followerId, UUID followeeId) {
+    this.followerId = followerId;
+    this.followeeId = followeeId;
+  }
+
+  public static Follow of(
+      UUID followerId,
+      UUID followeeId
+  ){
+    if(followerId.equals(followeeId)){
+      throw new IllegalArgumentException("자기 자신을 팔로우 할 수 없음");
+    }
+    return new Follow(followerId, followeeId);
+  }
+}
