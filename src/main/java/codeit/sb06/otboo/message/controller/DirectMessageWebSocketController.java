26876db1bbf2b1933b52f6693bc0123c8d1a4bc2
HOGUN00 <7080rjs@gmail.com>
2026-01-28 14:58:27 +0900

    feat: dm 키 생성 책임을 ChatRoom 엔티티로 넘기기

--- src/main/java/codeit/sb06/otboo/message/controller/DirectMessageWebSocketController.java
+++ src/main/java/codeit/sb06/otboo/message/controller/DirectMessageWebSocketController.java
@@ -1,6 +1,7 @@
 package codeit.sb06.otboo.message.controller;
 
 import codeit.sb06.otboo.message.dto.request.DirectMessageCreateRequest;
+import codeit.sb06.otboo.message.entity.ChatRoom;
 import codeit.sb06.otboo.message.service.DirectMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,11 +9,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
 
 @Slf4j
 @Controller
@@ -29,9 +25,8 @@ public class DirectMessageWebSocketController {
 
         log.debug("웹소켓 직접 메시지 전송 요청 받음: {}", request);
 
-        List<UUID> ids = Arrays.asList(request.senderId(), request.receiverId());
-        Collections.sort(ids);
-        String destination = "/sub/direct-messages_" + ids.get(0) + "_" + ids.get(1);
+        String dmKey = ChatRoom.generateDmKey(request.senderId(), request.receiverId());
+        String destination = "/sub/direct-messages_" + dmKey;
 
         directMessageService.create(request);
         messagingTemplate.convertAndSend(destination, request);
