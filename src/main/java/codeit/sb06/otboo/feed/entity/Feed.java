720c95e621f61ced14fafafe99e68c8fb2482732
SeoGyeongWon <skwknh00@naver.com>
2026-01-30 15:42:21 +0900

    feat: 엔티티, 레포지토리 추가

---
+++ src/main/java/codeit/sb06/otboo/feed/entity/Feed.java
@@ -0,0 +1,69 @@
+package codeit.sb06.otboo.feed.entity;
+
+import codeit.sb06.otboo.user.entity.Users;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "feeds")
+@Getter
+public class Feed {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "content", nullable = true)
+  private String content;
+
+  @Column(name = "like_count", nullable = false)
+  private Long likeCount;
+
+  @Column(name = "comment_count", nullable = false)
+  private int commentCount;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "author",nullable = false)
+  private Users user;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @Builder
+  public Feed(
+      String content,
+      Long likeCount,
+      int commentCount,
+      Users user,
+      LocalDateTime createdAt,
+      LocalDateTime updatedAt
+  ){
+    this.content = content;
+    this.likeCount = likeCount;
+    this.commentCount = commentCount;
+    this.user = user;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
