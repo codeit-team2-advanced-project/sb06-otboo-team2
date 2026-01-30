70aabab45432313a25f79227f1961d37b18071da
HOGUN00 <7080rjs@gmail.com>
2026-01-30 10:43:48 +0900

    refactor: 알림 서비스에 트랜잭셔널 어노테이션 추가

--- src/main/java/codeit/sb06/otboo/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/codeit/sb06/otboo/notification/service/impl/NotificationServiceImpl.java
@@ -9,12 +9,14 @@ import codeit.sb06.otboo.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
