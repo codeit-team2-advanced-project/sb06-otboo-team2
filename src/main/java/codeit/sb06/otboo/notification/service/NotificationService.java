653fad05a690b45108a8606c7d2b46193905b065
HOGUN00 <7080rjs@gmail.com>
2026-01-30 10:35:37 +0900

    refactor: 불필요한 import 문 제거

--- src/main/java/codeit/sb06/otboo/notification/service/NotificationService.java
+++ src/main/java/codeit/sb06/otboo/notification/service/NotificationService.java
@@ -1,7 +1,6 @@
 package codeit.sb06.otboo.notification.service;
 
 import codeit.sb06.otboo.notification.dto.NotificationDto;
-import codeit.sb06.otboo.notification.entity.Notification;
 import codeit.sb06.otboo.notification.enums.NotificationLevel;
 
 import java.util.UUID;
