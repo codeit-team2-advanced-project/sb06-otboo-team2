a039aa317c09baa0da20a33609df1cf0fa7d6778
SeoGyeongWon <skwknh00@naver.com>
2026-02-02 18:06:53 +0900

    feat: 쿼리 dsl로 댓글 목록 조회 동적 처리

--- src/main/java/codeit/sb06/otboo/comment/repository/CommentQueryRepositoryImpl.java
+++ src/main/java/codeit/sb06/otboo/comment/repository/CommentQueryRepositoryImpl.java
@@ -1,5 +1,7 @@
 package codeit.sb06.otboo.comment.repository;
 
+import static codeit.sb06.otboo.comment.entity.QComment.comment;
+
 import codeit.sb06.otboo.comment.entity.Comment;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -15,6 +17,14 @@ public class CommentQueryRepositoryImpl implements CommentQueryRepository {
 
   @Override
   public List<Comment> findCommentListByCursor(UUID feedId, UUID idAfter, int limit) {
-    return List.of();
+    var query = jpaQueryFactory.selectFrom(comment)
+        .where(comment.feed.id.eq(feedId))
+        .orderBy(comment.createdAt.asc())
+        .limit(limit);
+
+    if(idAfter != null){
+      query = query.where(comment.id.gt(idAfter));
+    }
+    return query.fetch();
   }
 }
