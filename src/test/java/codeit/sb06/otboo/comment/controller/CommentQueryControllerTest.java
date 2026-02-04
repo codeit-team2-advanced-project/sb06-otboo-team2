cb4b650bb4f9415d380198d6f3215110ad993633
SeoGyeongWon <skwknh00@naver.com>
2026-02-04 17:15:47 +0900

    fix: 테스트 전용 설정 추가

--- src/test/java/codeit/sb06/otboo/comment/controller/CommentQueryControllerTest.java
+++ src/test/java/codeit/sb06/otboo/comment/controller/CommentQueryControllerTest.java
@@ -23,11 +23,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CommentController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 public class CommentQueryControllerTest {
 
   @Autowired
