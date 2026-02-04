bcad98373607ed04e97ecd3722095b36444ef032
HOGUN00 <7080rjs@gmail.com>
2026-02-04 18:41:21 +0900

    test: 웹소켓 채널 인터셉터 테스트코드

---
+++ src/test/java/codeit/sb06/otboo/message/interceptor/WebSocketChannelInterceptorTest.java
@@ -0,0 +1,69 @@
+package codeit.sb06.otboo.message.interceptor;
+
+import codeit.sb06.otboo.exception.auth.InvalidTokenException;
+import codeit.sb06.otboo.security.jwt.JwtRegistry;
+import codeit.sb06.otboo.security.jwt.JwtTokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageBuilder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class WebSocketChannelInterceptorTest {
+
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Mock
+    private JwtRegistry jwtRegistry;
+
+    @Mock
+    private MessageChannel channel;
+
+    @InjectMocks
+    private WebSocketChannelInterceptor webSocketChannelInterceptor;
+
+    @Test
+    @DisplayName("웹소켓 메시지 전송 전 인터셉트 테스트")
+    void preSendTest() {
+        // given
+        StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.CONNECT);
+        accessor.addNativeHeader("Authorization", "Bearer dummyAccessToken");
+        Message<byte[]> message = MessageBuilder.createMessage(new byte[0], accessor.getMessageHeaders());
+        given(jwtTokenProvider.validateAccessToken(anyString())).willReturn(true);
+        given(jwtRegistry.hasActiveJwtInformationByAccessToken(anyString())).willReturn(true);
+
+        // when
+        Message<?> result = webSocketChannelInterceptor.preSend(message, channel);
+
+        // then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    @DisplayName("웹소켓 메시지 전송 전 인터셉트 - 유효하지 않은 토큰 테스트")
+    void preSendInvalidTokenTest() {
+        // given
+        StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.CONNECT);
+        accessor.addNativeHeader("Authorization", "Bearer invalidAccessToken");
+        Message<byte[]> message = MessageBuilder.createMessage(new byte[0], accessor.getMessageHeaders());
+        given(jwtTokenProvider.validateAccessToken(anyString())).willReturn(false);
+
+        // when & then
+        assertThatThrownBy(() ->
+                webSocketChannelInterceptor.preSend(message, channel))
+                .isInstanceOf(InvalidTokenException.class);
+    }
+}
