f2e5a627b8a89c355398213b04f4fc3b75062ac8
SeoGyeongWon <skwknh00@naver.com>
2026-02-04 18:58:53 +0900

    feat: 팔로우 생성 로직 구현

--- src/main/java/codeit/sb06/otboo/follow/service/BasicFollowService.java
+++ src/main/java/codeit/sb06/otboo/follow/service/BasicFollowService.java
@@ -2,13 +2,40 @@ package codeit.sb06.otboo.follow.service;
 
 import codeit.sb06.otboo.follow.dto.FollowCreateRequest;
 import codeit.sb06.otboo.follow.dto.FollowDto;
+import codeit.sb06.otboo.follow.dto.FolloweeDto;
+import codeit.sb06.otboo.follow.dto.FollowerDto;
+import codeit.sb06.otboo.follow.entity.Follow;
+import codeit.sb06.otboo.follow.repository.FollowRepository;
+import codeit.sb06.otboo.user.entity.User;
+import codeit.sb06.otboo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Service
 public class BasicFollowService implements FollowService {
 
+  private final FollowRepository followRepository;
+  private final UserRepository userRepository;
+
+  @Transactional
   @Override
-  public FollowDto createFollow(FollowCreateRequest FollowCreateRequest) {
-    return null;
+  public FollowDto createFollow(FollowCreateRequest followCreateRequest) {
+
+    User follower = userRepository.findById(followCreateRequest.followerId())
+        .orElseThrow();
+    User followee = userRepository.findById(followCreateRequest.followeeId())
+        .orElseThrow();
+
+    Follow follow = Follow.of(follower,followee);
+
+    followRepository.save(follow);
+
+    FolloweeDto followeeDto = new FolloweeDto(followee.getId(),followee.getName(), followee.getProfileImageUrl());
+    FollowerDto followerDto = new FollowerDto(follower.getId(), follower.getName(),follower.getProfileImageUrl());
+
+    return FollowDto.of(follow, followeeDto, followerDto);
   }
 }
