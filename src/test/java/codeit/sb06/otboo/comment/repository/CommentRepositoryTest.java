65eb52e21bc8bbe3020bf43a2059ff0d8ae00787
SeoGyeongWon <skwknh00@naver.com>
2026-02-05 14:07:08 +0900

    fix: 테스트 객체 시간 값 추가

--- src/test/java/codeit/sb06/otboo/comment/repository/CommentRepositoryTest.java
+++ src/test/java/codeit/sb06/otboo/comment/repository/CommentRepositoryTest.java
@@ -10,6 +10,7 @@ import codeit.sb06.otboo.feed.repository.FeedRepository;
 import codeit.sb06.otboo.user.dto.request.UserCreateRequest;
 import codeit.sb06.otboo.user.entity.User;
 import codeit.sb06.otboo.user.repository.UserRepository;
+import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -39,7 +40,7 @@ public class CommentRepositoryTest {
   private User user;
 
   @BeforeEach
-  void setUp() {
+  void setUp() throws NoSuchFieldException, IllegalAccessException {
     user = userRepository.save(
         User.from(new UserCreateRequest("테스트 유저", "codeit1234@otboo.com", "pwd123"))
     );
@@ -51,14 +52,20 @@ public class CommentRepositoryTest {
             .build()
     );
 
-    for (int i = 24; i >=0; i--) {
-      commentRepository.save(
-          Comment.builder()
-              .feed(feed)
-              .user(user)
-              .content("테스트 댓글 " + (i + 1))
-              .build()
-      );
+    for (int i = 0; i < 25; i++) {
+      Comment comment = Comment.builder()
+          .feed(feed)
+          .user(user)
+          .content("테스트 댓글 " + (i + 1))
+          .build();
+
+      commentRepository.save(comment);
+
+      Field createdAtField = Comment.class.getDeclaredField("createdAt");
+      createdAtField.setAccessible(true);
+      createdAtField.set(comment, LocalDateTime.now().minusMinutes(i));
+
+      commentRepository.save(comment);
     }
   }
 
@@ -66,16 +73,16 @@ public class CommentRepositoryTest {
   @Test
   void findFirstPage() {
 
-      //when
-     List<Comment> result =
-         commentRepository.findCommentListByCursor(feed.getId(), null,null, 20);
+    //when
+    List<Comment> result =
+        commentRepository.findCommentListByCursor(feed.getId(), null,null, 20);
 
-     // then
-      assertThat(result).hasSize(20);
-      assertThat(result.get(0).getContent()).isEqualTo("테스트 댓글 1");
-      assertThat(result.get(19).getContent()).isEqualTo("테스트 댓글 20");
+    // then
+    assertThat(result).hasSize(20);
+    assertThat(result.get(0).getContent()).isEqualTo("테스트 댓글 1");
+    assertThat(result.get(19).getContent()).isEqualTo("테스트 댓글 20");
 
-    }
+  }
 
   // 다음 페이지 조회
   @Test
@@ -98,5 +105,5 @@ public class CommentRepositoryTest {
     assertThat(nextPage.get(0).getContent()).isEqualTo("테스트 댓글 21");
     assertThat(nextPage.get(3).getContent()).isEqualTo("테스트 댓글 24");
 
-    }
+  }
 }
