e8e8dac97464c37e6681855a6d9f02f41eca2551
SeoGyeongWon <skwknh00@naver.com>
2026-02-05 18:39:11 +0900

    feat: 팔로우 요약 조회 구조 추가

--- src/main/java/codeit/sb06/otboo/follow/controller/FollowController.java
+++ src/main/java/codeit/sb06/otboo/follow/controller/FollowController.java
@@ -2,16 +2,22 @@ package codeit.sb06.otboo.follow.controller;
 
 import codeit.sb06.otboo.follow.dto.FollowCreateRequest;
 import codeit.sb06.otboo.follow.dto.FollowDto;
+import codeit.sb06.otboo.follow.dto.FollowSummaryDto;
 import codeit.sb06.otboo.follow.service.FollowService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,4 +41,18 @@ public class FollowController {
     return ResponseEntity.status(201)
         .body(response);
   }
+
+  @Operation(summary = "팔로우 요약 정보 조회")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "팔로우 요약 정보 조회 성공"),
+      @ApiResponse(responseCode = "400", description = "팔로우 조회 실패")
+  })
+  @GetMapping("/follows/summary")
+  public ResponseEntity<FollowSummaryDto> getFollowSummary(
+      @RequestParam UUID uuid
+  ){
+    return ResponseEntity.ok(null);
+  }
+
+
 }
--- src/main/java/codeit/sb06/otboo/follow/service/FollowService.java
+++ src/main/java/codeit/sb06/otboo/follow/service/FollowService.java
@@ -2,7 +2,10 @@ package codeit.sb06.otboo.follow.service;
 
 import codeit.sb06.otboo.follow.dto.FollowCreateRequest;
 import codeit.sb06.otboo.follow.dto.FollowDto;
+import codeit.sb06.otboo.follow.dto.FollowSummaryDto;
+import java.util.UUID;
 
 public interface FollowService {
   FollowDto createFollow(FollowCreateRequest followCreateRequest);
+  FollowSummaryDto getFollowSummary(UUID userId);
 }
