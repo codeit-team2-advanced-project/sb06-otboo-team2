610d71922e3e04a1f7ca1fcbbb88da09fb519028
SeoGyeongWon <skwknh00@naver.com>
2026-02-05 18:39:11 +0900

    feat: 팔로우 요약 조회 구조 추가

--- src/main/java/codeit/sb06/otboo/follow/service/BasicFollowService.java
+++ src/main/java/codeit/sb06/otboo/follow/service/BasicFollowService.java
@@ -5,12 +5,14 @@ import codeit.sb06.otboo.exception.user.UserException;
 import codeit.sb06.otboo.exception.user.UserNotFoundException;
 import codeit.sb06.otboo.follow.dto.FollowCreateRequest;
 import codeit.sb06.otboo.follow.dto.FollowDto;
+import codeit.sb06.otboo.follow.dto.FollowSummaryDto;
 import codeit.sb06.otboo.follow.dto.FolloweeDto;
 import codeit.sb06.otboo.follow.dto.FollowerDto;
 import codeit.sb06.otboo.follow.entity.Follow;
 import codeit.sb06.otboo.follow.repository.FollowRepository;
 import codeit.sb06.otboo.user.entity.User;
 import codeit.sb06.otboo.user.repository.UserRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,5 +42,10 @@ public class BasicFollowService implements FollowService {
     FollowerDto followerDto = new FollowerDto(follower.getId(), follower.getName(),follower.getProfileImageUrl());
 
     return FollowDto.of(follow, followeeDto, followerDto);
+  }
+
+  @Override
+  public FollowSummaryDto getFollowSummary(UUID userId) {
+    return null;
   }
 }
