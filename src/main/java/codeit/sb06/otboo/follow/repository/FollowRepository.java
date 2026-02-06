d472b482bf981a193479262da62f1999f33eed1f
SeoGyeongWon <skwknh00@naver.com>
2026-02-06 18:10:40 +0900

    feat: 팔로우 요약 조회 레포지토리 팔로워 팔로우 수 계산, 팔로우 조건 메서드 추가

--- src/main/java/codeit/sb06/otboo/follow/repository/FollowRepository.java
+++ src/main/java/codeit/sb06/otboo/follow/repository/FollowRepository.java
@@ -1,9 +1,15 @@
 package codeit.sb06.otboo.follow.repository;
 
 import codeit.sb06.otboo.follow.entity.Follow;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
+
+  Long countByFollowerId(UUID followerId);
+  Long countByFolloweeId(UUID followingId);
+
+  Optional<Follow> findByFollowerIdAndFolloweeId(UUID followerId, UUID followingId);
 
 }
