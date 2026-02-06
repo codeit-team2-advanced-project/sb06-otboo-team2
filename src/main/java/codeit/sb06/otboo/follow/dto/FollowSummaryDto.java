d3e2ca2338368db85b22d769697fb4c63745dd10
SeoGyeongWon <skwknh00@naver.com>
2026-02-06 18:12:29 +0900

    feat: 팔로우 요약 조회 dto 생성자 메서드 of 추가

--- src/main/java/codeit/sb06/otboo/follow/dto/FollowSummaryDto.java
+++ src/main/java/codeit/sb06/otboo/follow/dto/FollowSummaryDto.java
@@ -1,5 +1,7 @@
 package codeit.sb06.otboo.follow.dto;
 
+import codeit.sb06.otboo.follow.entity.Follow;
+import java.util.Optional;
 import java.util.UUID;
 
 public record FollowSummaryDto(
@@ -11,4 +13,20 @@ public record FollowSummaryDto(
     boolean followingMe
 ) {
 
+  public static FollowSummaryDto of(
+      UUID followeeId,
+      Long followerCount,
+      Long followingCount,
+      Optional<Follow> followedByMe,
+      Optional<Follow> followingMe
+  ) {
+    return new FollowSummaryDto(
+        followeeId,
+        followerCount,
+        followingCount,
+        followedByMe.isPresent(),
+        followedByMe.map(Follow::getId).orElse(null),
+        followingMe.isPresent()
+    );
+  }
 }
