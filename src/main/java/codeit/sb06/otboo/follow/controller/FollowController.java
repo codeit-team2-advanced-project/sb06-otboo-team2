05876d282bb832803b44aef3db38aa635700542c
SeoGyeongWon <skwknh00@naver.com>
2026-02-06 21:46:32 +0900

    feat: 팔로우 요약 조회 컨트롤러 응답 구현

--- src/main/java/codeit/sb06/otboo/follow/controller/FollowController.java
+++ src/main/java/codeit/sb06/otboo/follow/controller/FollowController.java
@@ -4,16 +4,15 @@ import codeit.sb06.otboo.follow.dto.FollowCreateRequest;
 import codeit.sb06.otboo.follow.dto.FollowDto;
 import codeit.sb06.otboo.follow.dto.FollowSummaryDto;
 import codeit.sb06.otboo.follow.service.FollowService;
+import codeit.sb06.otboo.security.CurrentUserId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,9 +48,12 @@ public class FollowController {
   })
   @GetMapping("/follows/summary")
   public ResponseEntity<FollowSummaryDto> getFollowSummary(
-      @RequestParam UUID uuid
+      @RequestParam UUID targetId,
+      @CurrentUserId UUID myId
+
   ){
-    return ResponseEntity.ok(null);
+    FollowSummaryDto response = followService.getFollowSummary(targetId, myId);
+    return ResponseEntity.ok(response);
   }
 
 
