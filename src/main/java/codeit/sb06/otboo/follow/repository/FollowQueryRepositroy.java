b2e2faa7cd0f42299ac491358b41ec6ec954437b
SeoGyeongWon <skwknh00@naver.com>
2026-02-09 15:23:50 +0900

    feat: 팔로워, 팔로잉 목록조회 커서 페이지네이션 구현

---
+++ src/main/java/codeit/sb06/otboo/follow/repository/FollowQueryRepositroy.java
@@ -0,0 +1,18 @@
+package codeit.sb06.otboo.follow.repository;
+
+import codeit.sb06.otboo.follow.entity.Follow;
+import codeit.sb06.otboo.follow.entity.FollowDirection;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface FollowQueryRepositroy {
+  List<Follow> findByCursor(
+      FollowDirection followDirection,
+      UUID userId,
+      LocalDateTime lastCreatedAt,
+      UUID idAfter,
+      int limit,
+      String nameLike
+  );
+}
