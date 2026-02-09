b3129326fbd9475879b596c7cbf8ab4c83d419f2
SeoGyeongWon <skwknh00@naver.com>
2026-02-09 15:23:50 +0900

    feat: 팔로워, 팔로잉 목록조회 커서 페이지네이션 구현

---
+++ src/main/java/codeit/sb06/otboo/follow/repository/FollowQueryRepositoryImpl.java
@@ -0,0 +1,57 @@
+package codeit.sb06.otboo.follow.repository;
+
+import static codeit.sb06.otboo.follow.entity.QFollow.follow;
+
+import codeit.sb06.otboo.follow.entity.Follow;
+import codeit.sb06.otboo.follow.entity.FollowDirection;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class FollowQueryRepositoryImpl implements FollowQueryRepositroy{
+
+  private final JPAQueryFactory queryFactory;
+
+  @Override
+  public List<Follow> findByCursor(FollowDirection followDirection, UUID userId,
+      LocalDateTime lastCreatedAt, UUID idAfter, int limit, String nameLike) {
+
+    var query = queryFactory.selectFrom(follow)
+        .join(follow.followee).fetchJoin()
+        .join(follow.follower).fetchJoin()
+        .orderBy(
+            follow.createdAt.desc(),
+            follow.id.desc()
+        )
+        .limit(limit);
+
+    if (followDirection == FollowDirection.FOllOWER) {
+      query.where(follow.followee.id.eq(userId));
+    }
+    else {
+      query.where(follow.follower.id.eq(userId));
+    }
+
+    if (lastCreatedAt != null && idAfter != null) {
+      query.where(
+          follow.createdAt.lt(lastCreatedAt)
+              .or(
+                  follow.createdAt.eq(lastCreatedAt)
+                      .and(follow.id.lt(idAfter))
+              )
+      );
+    }
+    if (nameLike != null && !nameLike.isBlank()) {
+      query.where(
+          follow.followee.name.containsIgnoreCase(nameLike)
+              .or(follow.follower.name.containsIgnoreCase(nameLike))
+      );
+    }
+    return query.fetch();
+  }
+}
