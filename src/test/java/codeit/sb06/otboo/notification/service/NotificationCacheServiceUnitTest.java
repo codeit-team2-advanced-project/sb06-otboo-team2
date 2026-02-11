1a12cb85955839d0ea48cdd49fed125f9766638f
HOGUN00 <7080rjs@gmail.com>
2026-02-11 14:59:27 +0900

    test: 알림 캐시 서비스 단위 테스트 추가

---
+++ src/test/java/codeit/sb06/otboo/notification/service/NotificationCacheServiceUnitTest.java
@@ -0,0 +1,127 @@
+package codeit.sb06.otboo.notification.service;
+
+import codeit.sb06.otboo.config.JacksonConfig;
+import codeit.sb06.otboo.exception.notification.NotificationMappingException;
+import codeit.sb06.otboo.notification.dto.NotificationDto;
+import codeit.sb06.otboo.notification.entity.Notification;
+import codeit.sb06.otboo.notification.mapper.NotificationMapper;
+import codeit.sb06.otboo.notification.repository.NotificationRepository;
+import codeit.sb06.otboo.notification.service.impl.NotificationCacheServiceImpl;
+import codeit.sb06.otboo.notification.util.SseEventIdGenerator;
+import codeit.sb06.otboo.util.EasyRandomUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationCacheServiceUnitTest {
+
+    private final EasyRandom easyRandom = EasyRandomUtil.getRandom();
+
+    @Mock
+    private NotificationRepository notificationRepository;
+
+    @Spy
+    private NotificationMapper notificationMapper;
+
+    @Spy
+    private SseEventIdGenerator sseEventIdGenerator;
+
+    @Mock
+    private StringRedisTemplate redisTemplate;
+
+    @Mock
+    private ListOperations<String, String> listOps;
+
+    @Mock
+    private JacksonConfig jacksonConfig;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private NotificationCacheServiceImpl notificationCacheService;
+
+    @BeforeEach
+    void setUp() {
+        given(redisTemplate.opsForList()).willReturn(listOps);
+    }
+
+    @Test
+    @DisplayName("dto 직렬화 실패 시 JsonProcessingException이 발생한다.")
+    void cacheNotificationTest() throws JsonProcessingException {
+        // given
+        NotificationDto dto = easyRandom.nextObject(NotificationDto.class);
+
+        given(objectMapper.writeValueAsString(dto))
+                .willThrow(new JsonProcessingException("Force Serialization Error") {
+                });
+
+        // when & then
+        assertThatThrownBy(() -> notificationCacheService.save(dto))
+                .isInstanceOf(NotificationMappingException.class);
+    }
+
+    @Test
+    @DisplayName("알림 캐시에 데이터가 없으면 db에서 조회하고 캐시에 저장한다.")
+    void getNotificationsAfterTest() {
+        // given
+        UUID userId = UUID.randomUUID();
+        String key = "notifications:user:" + userId;
+        given(listOps.range(key, 0, -1))
+                .willReturn(List.of());
+
+        List<Notification> dtoList = easyRandom.objects(Notification.class, 3).toList();
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Slice<Notification> slice = new SliceImpl<>(dtoList, pageRequest, false);
+        given(notificationRepository.findFirstPageByReceiverId(any(), any()))
+                .willReturn(slice);
+
+        // when
+        List<NotificationDto> response = notificationCacheService.getRecentNotifications(userId);
+
+        // then
+        assertThat(response).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("json으로부터 dto 변환 실패 시 NotificationMappingException이 발생한다.")
+    void getNotificationsAfterMappingExceptionTest() throws JsonProcessingException {
+        // given
+        UUID userId = UUID.randomUUID();
+        String key = "notifications:user:" + userId;
+        String invalidJson = "invalid json string";
+
+        given(listOps.range(key, 0, -1))
+                .willReturn(List.of(invalidJson));
+
+        given(objectMapper.readValue(invalidJson, NotificationDto.class))
+                .willThrow(new JsonProcessingException("Force Deserialization Error") {
+                });
+
+        // when & then
+        assertThatThrownBy(() -> notificationCacheService.getRecentNotifications(userId))
+                .isInstanceOf(NotificationMappingException.class);
+    }
+}
