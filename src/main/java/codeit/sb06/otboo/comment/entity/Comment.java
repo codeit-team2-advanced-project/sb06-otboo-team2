3b4ea3d0065bb704dc67b48d6560725c1a6e4dda
SeoGyeongWon <skwknh00@naver.com>
2026-02-19 10:08:50 +0900

    refactor: 엔티티 리팩토링 - updatedAt 미사용으로 삭제, 연관 관계 fetchType 추가

--- src/main/java/codeit/sb06/otboo/comment/entity/Comment.java
+++ src/main/java/codeit/sb06/otboo/comment/entity/Comment.java
@@ -19,7 +19,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -40,15 +39,11 @@ public class Comment {
   @Column(name = "created_at",nullable = false)
   private LocalDateTime createdAt;
 
-  @LastModifiedDate
-  @Column(name = "updated_at",nullable = false)
-  private LocalDateTime updatedAt;
-
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "feed_id",nullable = false)
   private Feed feed;
 
